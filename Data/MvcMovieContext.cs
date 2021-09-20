25bce7d008b72891cd34176a2fe1e7f737d7079b
NGAN DAM THANH <1821050429@student.humg.edu.vn>
2021-09-20 23:54:22 +0700

    First commit

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using demonet.Models;
+
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<demonet.Models.Movie> Movie { get; set; }
+
+        public DbSet<demonet.Models.Student> Student { get; set; }
+    
+    }
