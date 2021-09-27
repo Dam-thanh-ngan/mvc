45829b1bbc73f1fd4c3a426f5ed312bae1d69af1
NGAN DAM THANH <1821050429@student.humg.edu.vn>
2021-09-27 08:06:38 +0700

    lan3 commit

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,6 +6,7 @@ namespace demonet.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="Title is required.")]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
